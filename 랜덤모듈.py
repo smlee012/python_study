02723f5f26faae0e82b774fc8efcea25c63010c2
SMLEE <lsm643@naver.com>
2021-11-15 15:19:53 +0900

    11월 15일

--- 랜덤모듈.py
+++ 랜덤모듈.py
@@ -1,5 +1,6 @@
 # 랜덤 모듈 불러오기
 import random
+import myModule as my
 
 x = random.randint(0,1) # 0,1의 랜덤 숫자 생성
 # 동전을 던졌을때 랜덤으로 앞 뒷면이 나오도록 코드 작성
@@ -7,3 +8,5 @@ if x == 1:
     print("앞면")
 else:
     print("뒷면")
+
+print(my.pi)
